6f6aec213bd95abecb4ded60b60fa0a42d707ee0
Hugo Ferreira <hugoferreirait@gmail.com>
2021-06-04 07:43:17 -0300

    Fixing bug at Side Bar Menu

--- js/sidemenu.js
+++ js/sidemenu.js
@@ -2,7 +2,7 @@
 
 const menuBtnContainer = document.querySelector('.menu_btn_container')
 const menuBtn = document.querySelector('.menu_btn')
-let menuOpen = false
+var menuOpen = false
 const sideBarMenu = document.getElementById('sidebar_menu')
 const backgroundMenu = document.getElementById('menu_close')
 const navItemsList = document.getElementById('nav_item_list')
@@ -46,4 +46,45 @@ backgroundMenu.addEventListener('click', () => {
     navItemsList.style.transition = '0.1s'
     menuBtn.classList.remove('open')
     menuOpen = false
+})
+
+const shopButton = document.getElementById('shop_btn')
+const shopMenuButton = document.getElementById('shop_menu_btn')
+const shopBackground = document.getElementById('shop_background')
+const shopContainer = document.getElementById('shop_container')
+const closeButton = document.getElementById('close_button')
+const body = document.getElementById('body')
+
+shopButton.addEventListener('click', () => {
+    shopBackground.style.opacity = '1'
+    shopBackground.style.visibility = 'visible'
+    shopContainer.style.opacity = '1'
+    shopContainer.style.visibility = 'visible'
+    closeButton.style.transform = 'translateX(0)'
+    body.style.overflow = 'hidden'
+})
+
+shopMenuButton.addEventListener('click', () => {
+
+    shopBackground.style.opacity = '1'
+    shopBackground.style.visibility = 'visible'
+    shopContainer.style.opacity = '1'
+    shopContainer.style.visibility = 'visible'
+    sideBarMenu.style.width = '0'
+    backgroundMenu.style.opacity = '0'
+    backgroundMenu.style.visibility = 'hidden'
+    navItemsList.style.opacity = '0'
+    navItemsList.style.visibility = 'hidden'
+    navItemsList.style.transition = '0.1s'
+    menuBtn.classList.remove('open')
+    body.style.overflow = 'hidden'
+    menuOpen = false
+})
+
+closeButton.addEventListener('click', () => {
+    shopBackground.style.opacity = '0'
+    shopBackground.style.visibility = 'hidden'
+    shopContainer.style.opacity = '0'
+    shopContainer.style.visibility = 'hidden'
+    body.style.overflow = 'visible'
 })
