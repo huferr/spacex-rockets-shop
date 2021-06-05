5a04a872376d30781027265aa88fb78875d1444e
Hugo Ferreira <hugoferreirait@gmail.com>
2021-06-05 20:37:04 -0300

    Login interations

--- js/login.js
+++ js/login.js
@@ -16,21 +16,32 @@ export function login() {
                 backgroundLogin.style.opacity = 0
                 loginContainer.style.opacity = 0
             })
-
-            
-            const loginButton = document.getElementById('login_button')
-
-            loginButton.addEventListener('click', () => {
-                
-                let emailInput = document.getElementById('email_input')
-                let passwordInput = document.getElementById('password_input')
-
-                console.log('it works fine')
-                
-                
-            })
-
         })
     }
-    
 }
+
+const loginButton = document.getElementById('login_button')
+
+    loginButton.addEventListener('click', () => {
+        
+        let emailInput = document.getElementById('email_input')
+        let passwordInput = document.getElementById('password_input')
+        
+        if(emailInput.value === "elonmusk@spacex.com" & passwordInput.value === "ilovedogecoin"){
+
+            displayMessage('success',"Now you're Logged in!")
+
+        } else if(emailInput.value != 'elonmusk@spacex.com' || passwordInput.value != "ilovedogecoin") {
+
+            displayMessage('error', 'Invalid Email/Password, please Try Again!')
+        }
+        
+    })
+
+function displayMessage(situation, message) {
+    let loginMessage = document.getElementById('login_msg')
+
+    loginMessage.classList.remove('login_msg_success', 'login_msg_error')
+    loginMessage.classList.add(`login_msg_${situation}`)
+    loginMessage.innerHTML = message
+}    
