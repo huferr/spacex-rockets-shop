93222b14aeb0a16c2f20204088fab1dc7841fc67
Hugo Ferreira <hugoferreirait@gmail.com>
2021-06-04 22:00:47 -0300

    Login Interactions and Project Refactoring

--- js/login.js
+++ js/login.js
@@ -4,7 +4,10 @@ export function login() {
     for (let i = 0; i < buy.length; i++) {
         buy[i].addEventListener('click', () => {
             const backgroundLogin = document.getElementById('background_login')
+            const loginContainer = document.getElementById('login_container')
             backgroundLogin.style.visibility = 'visible'
+            backgroundLogin.style.opacity = 1
+            loginContainer.style.opacity = 1
         })
     }
     
