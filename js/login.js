b29a41b18224ffa16ae7c8a57a3bffe032a85518
Hugo Ferreira <hugoferreirait@gmail.com>
2021-06-05 09:39:41 -0300

    Login form Environment

--- js/login.js
+++ js/login.js
@@ -5,9 +5,31 @@ export function login() {
         buy[i].addEventListener('click', () => {
             const backgroundLogin = document.getElementById('background_login')
             const loginContainer = document.getElementById('login_container')
+            const closeLoginButton = document.getElementById('close_login_btn')
+            
             backgroundLogin.style.visibility = 'visible'
             backgroundLogin.style.opacity = 1
             loginContainer.style.opacity = 1
+
+            closeLoginButton.addEventListener('click', () => {
+                backgroundLogin.style.visibility = 'hidden'
+                backgroundLogin.style.opacity = 0
+                loginContainer.style.opacity = 0
+            })
+
+            
+            const loginButton = document.getElementById('login_button')
+
+            loginButton.addEventListener('click', () => {
+                
+                let emailInput = document.getElementById('email_input')
+                let passwordInput = document.getElementById('password_input')
+
+                console.log('it works fine')
+                
+                
+            })
+
         })
     }
     
