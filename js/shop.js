54f3f729d6f62d61395ae9c42a73460ebf8cdf25
Hugo Ferreira <hugoferreirait@gmail.com>
2021-06-06 09:53:17 -0300

    array changes

--- js/shop.js
+++ js/shop.js
@@ -1,36 +1,64 @@
 const rockets = [
-    {name: "Falcon 9", price: 90, speed: "10.500 km/h", image: "./images/rockets/falcon9.png" },
-    {name: "Falcon Heavy", price: 120, speed: "15.000 km/h", image: "./images/rockets/falconheavy.png"},
-    {name: "Saturn V", price: 40, speed: "5.000 km/h", image: "./images/rockets/saturnv.png"},
-    {name: "Ariane 5", price: 45, speed: "6.000 km/h", image: "./images/rockets/ariane5.png"},
-    {name: "Energia", price: 20, speed: "3.000 km/h", image: "./images/rockets/energia.png"},
-    {name: "Proton", price: 60, speed: "8.000 km/h", image: "./images/rockets/proton.png"},
-    {name: "PSLV", price: 10, speed: "1.500 km/h", image: "./images/rockets/pslv.png"},
-    {name: "SPUTNIK", price: 70, speed: "9.000 km/h", image: "./images/rockets/sputnik.png"},
-    {name: "KOSMOS-3M", price: 75, speed: "8.500 km/h", image: "./images/rockets/kosmos-3m.png"}
+    {
+        name: "Falcon 9", 
+        price: 90, 
+        speed: "10.500 km/h", 
+        image: "./images/rockets/falcon9.png"
+    },
+    {
+        name: "Falcon Heavy", 
+        price: 120, 
+        speed: "15.000 km/h", 
+        image: "./images/rockets/falconheavy.png"
+    },
+    {
+        name: "Saturn V", 
+        price: 40, 
+        speed: "5.000 km/h", 
+        image: "./images/rockets/saturnv.png"
+    },
+    {
+        name: "Ariane 5", 
+        price: 45, 
+        speed: "6.000 km/h", 
+        image: "./images/rockets/ariane5.png"
+    },
+    {
+        name: "Energia", 
+        price: 20, 
+        speed: "3.000 km/h", 
+        image: "./images/rockets/energia.png"
+    },
+    {
+        name: "Proton", 
+        price: 60, 
+        speed: "8.000 km/h", 
+        image: "./images/rockets/proton.png"
+    },
+    {
+        name: "PSLV", 
+        price: 10, 
+        speed: "1.500 km/h", 
+        image: "./images/rockets/pslv.png"
+    },
+    {
+        name: "SPUTNIK", 
+        price: 70, 
+        speed: "9.000 km/h", 
+        image: "./images/rockets/sputnik.png"
+    },
+    {
+        name: "KOSMOS-3M", 
+        price: 75, 
+        speed: "8.500 km/h", 
+        image: "./images/rockets/kosmos-3m.png"
+    }
 ]
-
-
 
 const maiorValorFilter = document.getElementById('maior_valor_filter')
 const menorValorFilter = document.getElementById('menor_valor_filter')
 const searchInput = document.getElementById('search_input')
 
-
-
-menorValorFilter.addEventListener('click', () => {
-    let filter = rockets.sort((a, b) => {
-        return a.price - b.price
-    })
-    displayRockets(filter)
-})
-
-maiorValorFilter.addEventListener('click', () => {
-    let filter = rockets.sort((a, b) => {
-        return b.price - a.price
-    })
-    displayRockets(filter)
-})
 
 searchInput.addEventListener('keyup', (words) => {
     let wordEntered = words.target.value.toLowerCase()
@@ -72,6 +100,20 @@ const displayRockets = (allRockets) => {
 }
 
 import {login} from './login.js'
+
+menorValorFilter.addEventListener('click', () => {
+    let filter = rockets.sort((a, b) => {
+        return a.price - b.price
+    })
+    displayRockets(filter)
+})
+
+maiorValorFilter.addEventListener('click', () => {
+    let filter = rockets.sort((a, b) => {
+        return b.price - a.price
+    })
+    displayRockets(filter)
+})
 
 loadRockets()
 
